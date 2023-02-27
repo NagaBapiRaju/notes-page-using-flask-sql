ba68171d66f246c2a9666ef5119dc2cf8467d34e
BapiRaju Pallapothu <98650015+NagaBapiRaju@users.noreply.github.com>
2023-02-27 08:53:29 +0000

    fghj

--- FlaskSQL/website/__init__.py
+++ FlaskSQL/website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sgfhkjszdgfgskdfg'
-    app.config['SQLALCHEMY_DATABASE_URI'] = F'sqlite:///FlaskSQL/my_instance/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = F'sqlite:///my_instance/{DB_NAME}'
     db.init_app(app)
 
     from .views import views
